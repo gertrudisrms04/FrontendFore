c85821e875ff4cc3a417a67daff57bc8ae5daacd
gertrudisrms04 <166855913+gertrudisrms04@users.noreply.github.com>
2025-04-30 16:39:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+        const toggleBtn = document.querySelector('.toggle-btn');
+        const navMenu = document.querySelector('.nav-menu');
+
+        toggleBtn.addEventListener('click', function () {
+            navMenu.classList.toggle('active')
+        });
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mainMenu = document.querySelector('.main-head');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 10) {
+            mainMenu.classList.add('slidedown');
+        } else {
+            mainMenu.classList.remove('slidedown');
+        }
+    });
+});
+
+let current = 0;
+const slides = document.querySelectorAll('.showcase .container');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+function prevSlide() {
+  current = (current - 1) % slides.length;
+  showSlide(current);
+}
+
+function showSection(id) {
+    document.querySelectorAll('.page-section').forEach(el => el.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
+  }
